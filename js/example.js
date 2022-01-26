036bede0abd3eaa91289737d5ecacdd150fa71d6
rammfall <15bogdan17@gmail.com>
2022-01-27 01:56:44 +0200

    test

--- js/example.js
+++ js/example.js
@@ -1,10 +1,10 @@
-const array = [             1, 2,  3];
+const array = [1, 2,     3];
 
 const mapResult = map(array, function (item, index, array) {
   return item * 4;
 });
 
-console.log(mapResult) // => [4, 8, 12]
+console.log(mapResult); // => [4, 8, 12]
 
 const filterResult = filter(array, (item, index, array) => {
   return item > 1;
@@ -12,20 +12,21 @@ const filterResult = filter(array, (item, index, array) => {
 
 console.log(filterResult); // => [2, 3]
 
-
-const reduceResult = reduce(array, (previous, current, index, array) => {
-  return previous + current;
-}, 0);
+const reduceResult = reduce(
+  array,
+  (previous, current, index, array) => {
+    return previous + current;
+  },
+  0
+);
 
 console.log(reduceResult); // => 6
-
 
 const someResult = some(array, (item, index, array) => {
   return item > 2;
 });
 
 console.log(someResult); // => true
-
 
 const everyResult = every(array, (item, index, array) => {
   return item > 2;
